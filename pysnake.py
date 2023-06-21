aceeef2d28a63a000ce4c84cb430d81419544bc4
1-2-3-bossman <satadenli@gmail.com>
2023-06-21 09:00:46 +0100

    Snake completed

--- pysnake.py
+++ pysnake.py
@@ -1,5 +1,6 @@
 import pygame 
 import sys
+import random
 
 WIDTH = 720
 HEIGHT = 720
@@ -9,29 +10,112 @@ pygame.init()
 screen = pygame.display.set_mode((720, 720))
 clock = pygame.time.Clock()
 
-def main():
-    grid = [[0 for i in range(24)] for j in range(24)]
+class Snake():
+    def __init__(self):
+        self.x = 4
+        self.y = 12
+        self.veloY = 0
+        self.veloX = 0
+        self.length = 1
+        self.occupies = [(4,12,0)]
 
-    #0 = empty, 1 = snake, 2 = apple
-    grid[4][12] = 1 #starting square
-    grid[16][12] = 2 # first apple
+    def update(self, keyEvent, grow,apple):
+        if (self.x>24 or self.x < 0 or self.y > 24 or self.y < 0):
+            self.gameOver()
+            apple.gameOver()
+        self.occupies.append((self.x,self.y,1 if keyEvent else 0))
+        self.x += self.veloX
+        self.y += self.veloY
+        if not grow:
+            self.occupies.pop(0)
+        self.length += 1 if grow else self.length
+
+
+    def gameOver(self,):
+        self.x = 4
+        self.y = 12
+        self.veloY = 0
+        self.veloX = 0
+        self.length = 1
+        self.occupies = [(4,12)]
+
+
+class Apple():
+    def __init__(self):
+        self.x = 16
+        self.y = 12
+    
+    def update(self, grid):
+        self.newX, self.newY = random.randint(0,23), random.randint(0,23)
+        while grid[self.newX][self.newY] != 0:
+            self.newX, self.newY = random.randint(0,23), random.randint(0,23)
+        self.x = self.newX
+        self.y = self.newY
+    
+    def gameOver(self):
+        self.x = 16
+        self.y = 12
+
+
+
+def main():
+
+    snake = Snake()
+    apple = Apple()
 
     while True:
+
+        grid = [[0 for i in range(24)] for j in range(24)]
+        keyEvent = False
+        grow = False
+
+        #0 = empty, 1 = snake, 2 = apple
+        for point in snake.occupies:
+            grid[point[0]][point[1]] = 1
+        grid[apple.x][apple.y] = 2
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a and snake.veloX == 0:
+                    snake.veloX = -1
+                    snake.veloY = 0
+                    keyEvent = True
+                elif event.key == pygame.K_d and snake.veloX == 0:
+                    snake.veloX = 1
+                    snake.veloY = 0
+                    keyEvent = True
+                elif event.key == pygame.K_w and snake.veloY == 0:
+                    snake.veloY = -1
+                    snake.veloX = 0
+                    keyEvent = True
+                elif event.key == pygame.K_s and snake.veloY == 0:
+                    snake.veloY = 1
+                    snake.veloX = 0
+                    keyEvent = True
+
         i = 0
         for x in range(0,WIDTH,BLOCKSIZE):
             j = 0
             for y in range(0,HEIGHT,BLOCKSIZE):
                 block = pygame.Rect(x,y,BLOCKSIZE,BLOCKSIZE)
+               # if x == 0:
+                #    pygame.draw.rect(screen,(255,255,255),block)
+                #else:
                 pygame.draw.rect(screen,blockColour(grid,i,j),block)
                 j += 1
-            i += 1         
+            i += 1
 
+        if snake.x == apple.x and snake.y == apple.y:
+            grow = True
+            snake.update(keyEvent,grow,apple)
+            apple.update(grid)
+        else:
+            snake.update(keyEvent, grow,apple)
+        
         pygame.display.update()
-        clock.tick(30) 
+        clock.tick(15) 
 
 
 def blockColour(grid,i,j):
